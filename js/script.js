017ab038d9a76daef4c33daec9257af2f927eb6f
Mathys97459 <mathys.dezitter@outlook.com>
2023-12-19 00:10:42 +0100

    header scroll

--- js/script.js
+++ js/script.js
@@ -18,3 +18,25 @@ window.addEventListener('scroll', function() {
         navTop.style.backgroundColor = 'transparent'; // Retour à la couleur transparente lorsque la page est au-dessus de la hauteur de la barre de navigation
     }
 });
+
+const navBottom = document.querySelector('.nav-bottom');
+
+// Ajoutez un gestionnaire d'événements pour le scroll
+window.addEventListener('scroll', function() {
+    // Obtenez la position actuelle de défilement verticale
+    const scrollPosition = window.scrollY;
+
+    // Déterminez la position de votre choix
+    if (scrollPosition >=35){
+        navBottom.style.position = "fixed";
+        navBottom.style.marginTop = 30 + "px";
+        navBottom.style.width = 90 + "%";
+    }else{
+        navBottom.style.position = "absolute";
+        navBottom.style.marginTop = 4 + "rem";
+        navBottom.style.width = 90 + "%";
+    }
+
+    // Modifiez la position de .nav-bottom en fonction de la position de défilement
+    
+});
