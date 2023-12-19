905815861b35fcb170ad96532a3e3ab449354794
Mathys97459 <mathys.dezitter@outlook.com>
2023-12-19 01:20:01 +0100

    header responsive

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
-function toggleMenu() {
-    var navbarLinks = document.getElementById("navbarLinks");
-    if (navbarLinks.style.display === "block") {
-        navbarLinks.style.display = "none";
-    } else {
-        navbarLinks.style.display = "block";
-    }
-}
+// Ajoutez ceci à votre fichier JS
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.nav-bottom').classList.toggle('menu-active');
+  });
+  
 
 //SCROLL MENU
-window.addEventListener('scroll', function() {
-    const nav = document.querySelector('.nav');
-    const navTop = document.querySelector('.nav-top');
-    console.log(window.scrollY)
-    if (window.scrollY > 50) {
-        navTop.style.backgroundColor = '#000'; // Changez la couleur ici
-    } else {
-        navTop.style.backgroundColor = 'transparent'; // Retour à la couleur transparente lorsque la page est au-dessus de la hauteur de la barre de navigation
-    }
-});
-
 const navBottom = document.querySelector('.nav-bottom');
 
 // Ajoutez un gestionnaire d'événements pour le scroll
